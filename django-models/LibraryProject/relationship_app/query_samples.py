dd9216fcd754a61162d55a46c7836f18acdb93d4
nanasarpp <nanasarpp@gmail.com>
2025-09-07 23:00:13 +0000

    Let us see

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,22 @@
+import os
+import django
+
+# Setup Django environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_models.settings')
+django.setup()
+
+from relationship_app.models import Author, Book, Library
+
+# Query all books by a specific author
+author = Author.objects.get(name="Chinua Achebe")
+books_by_author = Book.objects.filter(author=author)
+print(f"Books by {author.name}: {[book.title for book in books_by_author]}")
+
+# List all books in a library
+library = Library.objects.get(name="Central Library")
+books_in_library = library.books.all()
+print(f"Books in {library.name}: {[book.title for book in books_in_library]}")
+
+# Retrieve the librarian for a library
+librarian = library.librarian
+print(f"Librarian for {library.name}: {librarian.name}")
